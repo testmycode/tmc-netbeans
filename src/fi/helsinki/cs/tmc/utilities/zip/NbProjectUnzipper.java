7ec05b0e338fe0da34fa69ee415b8b36e3d252ef
Martin Pärtel <martin.partel@gmail.com>
2011-07-27 20:30:30 +0300

    (removed some old methods)

--- src/fi/helsinki/cs/tmc/utilities/zip/NbProjectUnzipper.java
+++ src/fi/helsinki/cs/tmc/utilities/zip/NbProjectUnzipper.java
@@ -5,7 +5,6 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
@@ -79,63 +78,5 @@ public class NbProjectUnzipper {
     
     private ZipInputStream readZip(byte[] data) {
         return new ZipInputStream(new ByteArrayInputStream(data));
-    }
-    
-    @Deprecated
-    public void unZip(byte[] data, String unzipPath, String projectName) throws IOException {
-        unZip(new ByteArrayInputStream(data), unzipPath);
-    }
-
-    /**
-     * Unzips a file to the same folder as the original zipped file.
-     * @param in Stream to read.
-     * @param unzipPath Should be a full path without "/" at the end.
-     * @return Name of the first unzipped file, should be a directory. [TODO: remove]
-     * @throws IOException
-     */
-    @Deprecated
-    public String unZip(InputStream in, String unzipPath) throws IOException {
-        unzipPath += File.separator;
-        (new File(unzipPath)).mkdirs();
-        ZipInputStream zin = new ZipInputStream(in);
-        ZipEntry entry = null;
-        byte[] buffer = new byte[1024];
-
-        String firstFile = null;  //The first file that is being unzipped should be the projects parent folder.
-        boolean first = true;
-
-        while ((entry = zin.getNextEntry()) != null) {
-
-            if (first) {
-                firstFile = entry.getName();
-                first = false;
-            }
-
-            (new File(unzipPath + entry.getName())).mkdirs();
-
-            if (entry.isDirectory()) {
-                continue;
-            }
-
-            File currentFile = new File(unzipPath + entry.getName());
-            if (currentFile.exists()) {
-                if (!currentFile.delete()) {
-                    throw new IOException("Unable to overwrite an old file!");
-                }
-            }
-
-            FileOutputStream out = new FileOutputStream(currentFile);
-            int len;
-
-            while ((len = zin.read(buffer)) >= 0) {
-                out.write(buffer, 0, len);
-            }
-
-            zin.closeEntry();
-            out.close();
-        }
-        zin.close();
-        return firstFile;
-
     }
 }
