bef79623cd0dd16cc2aa3b2653993a9aed5d3a6c
Martin Pärtel <martin.partel@gmail.com>
2012-08-29 16:06:41 +0300

    Remove old and misnamed update center on plugin upgrade.

--- tmc-plugin/src/fi/helsinki/cs/tmc/actions/TmcModuleInstall.java
+++ tmc-plugin/src/fi/helsinki/cs/tmc/actions/TmcModuleInstall.java
@@ -4,14 +4,25 @@ import fi.helsinki.cs.tmc.model.CourseDb;
 import fi.helsinki.cs.tmc.model.ServerAccess;
 import fi.helsinki.cs.tmc.spyware.SpywareFacade;
 import fi.helsinki.cs.tmc.ui.LoginDialog;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.prefs.Preferences;
+import org.netbeans.api.autoupdate.UpdateUnitProvider;
+import org.netbeans.api.autoupdate.UpdateUnitProviderFactory;
+import org.openide.modules.ModuleInfo;
 import org.openide.modules.ModuleInstall;
+import org.openide.modules.Modules;
+import org.openide.modules.SpecificationVersion;
 import org.openide.util.Lookup;
 import org.openide.util.NbPreferences;
 import org.openide.windows.WindowManager;
 
 public class TmcModuleInstall extends ModuleInstall {
     private static final String PREF_FIRST_RUN = "firstRun";
+    private static final String PREF_MODULE_VERSION = "moduleVersion";
+    
+    private static final Logger log = Logger.getLogger(TmcModuleInstall.class.getName());
     
     @Override
     public void restored() {
@@ -19,6 +30,13 @@ public class TmcModuleInstall extends ModuleInstall {
             @Override
             public void run() {
                 Preferences prefs = NbPreferences.forModule(TmcModuleInstall.class);
+                
+                SpecificationVersion currentVersion = getCurrentModuleVersion();
+                SpecificationVersion prevVersion = new SpecificationVersion(prefs.get(PREF_MODULE_VERSION, "0.0.0"));
+                if (!currentVersion.equals(prevVersion)) {
+                    doUpdateFromPreviousVersion(prevVersion);
+                    prefs.put(PREF_MODULE_VERSION, currentVersion.toString());
+                }
                 
                 boolean isFirstRun = prefs.getBoolean(PREF_FIRST_RUN, true);
                 if (isFirstRun) {
@@ -39,6 +57,11 @@ public class TmcModuleInstall extends ModuleInstall {
             }
         });
     }
+    
+    private SpecificationVersion getCurrentModuleVersion() {
+        ModuleInfo modInfo = Modules.getDefault().ownerOf(this.getClass());
+        return modInfo.getSpecificationVersion();
+    }
 
     @Override
     public void close() {
@@ -47,5 +70,29 @@ public class TmcModuleInstall extends ModuleInstall {
     
     private void doFirstRun() {
         new ShowSettingsAction().run();
+    }
+    
+    private void doUpdateFromPreviousVersion(SpecificationVersion prevVersion) {
+        // A previous update center was registered with an incorrect name in versions before 0.3.1.
+        // Moreover the URL has since changed. This version provides the new update center,
+        // and here we ensure here that the old one is removed.
+        if (prevVersion.compareTo(new SpecificationVersion("0.3.1")) <= 0) {
+            removeOldUpdateCenterFromBefore031();
+        }
+    }
+    
+    private void removeOldUpdateCenterFromBefore031() {
+        ArrayList<UpdateUnitProvider> providersToRemove = new ArrayList<UpdateUnitProvider>();
+        for (UpdateUnitProvider provider : UpdateUnitProviderFactory.getDefault().getUpdateUnitProviders(false)) {
+            String url = provider.getProviderURL().toString();
+            if (url.startsWith("http://tmc.mooc.fi/updates") && !url.contains("tmc-netbeans-author")) {
+                log.log(Level.INFO, "Removing obsolete update center: {0}", provider.getDisplayName());
+                providersToRemove.add(provider);
+            }
+        }
+        
+        for (UpdateUnitProvider provider : providersToRemove) {
+            UpdateUnitProviderFactory.getDefault().remove(provider);
+        }
     }
 }
