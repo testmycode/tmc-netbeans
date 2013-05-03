c805881cdabba5c2363108815d826c7a7e0ea35a
Kalle Viiri <kviiri@cs.helsinki.fi>
2013-05-04 01:25:03 +0300

    remove netbeans template residue

--- tmc-plugin/src/fi/helsinki/cs/tmc/actions/PastebinAction.java
+++ tmc-plugin/src/fi/helsinki/cs/tmc/actions/PastebinAction.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package fi.helsinki.cs.tmc.actions;
 
 import fi.helsinki.cs.tmc.data.Exercise;
