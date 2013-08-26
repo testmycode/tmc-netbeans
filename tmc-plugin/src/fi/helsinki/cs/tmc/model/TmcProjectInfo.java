dcb2ac5f7c3895a743e0d6610ec6fe50a22b028e
avihavai <avihavai@XX.helsinki.fi>
2013-08-26 22:14:34 +0300

    functionality to exclude directories from snapshots: if a file called .tmcnosubmit exists in a directory, the directory will not be included in a snapshot

--- tmc-plugin/src/fi/helsinki/cs/tmc/model/TmcProjectInfo.java
+++ tmc-plugin/src/fi/helsinki/cs/tmc/model/TmcProjectInfo.java
@@ -13,40 +13,41 @@ import org.openide.filesystems.FileUtil;
  * Carries information about a project used in TMC.
  */
 public class TmcProjectInfo {
+
     private Project project;
-    
+
     /*package*/ TmcProjectInfo(Project project) {
         this.project = project;
     }
-    
+
     public Project getProject() {
         return project;
     }
-    
+
     public String getProjectName() {
         return ProjectUtils.getInformation(project).getDisplayName();
     }
-    
+
     public FileObject getProjectDir() {
         return project.getProjectDirectory();
     }
-    
+
     public File getProjectDirAsFile() {
         return FileUtil.toFile(getProjectDir());
     }
-    
+
     public String getProjectDirAbsPath() {
         return FileUtil.toFile(getProjectDir()).getAbsolutePath();
     }
-    
+
     public boolean isOpen() {
         return OpenProjects.getDefault().isProjectOpen(project);
     }
-    
+
     public TmcProjectFile getTmcProjectFile() {
         return TmcProjectFile.forProject(FileUtil.toFile(getProjectDir()));
     }
-    
+
     //TODO: a more robust/elegant/extensible project type recognition system
     public TmcProjectType getProjectType() {
         String pd = getProjectDirAbsPath();
@@ -58,51 +59,66 @@ public class TmcProjectInfo {
             return TmcProjectType.JAVA_SIMPLE;
         }
     }
-    
+
     public RecursiveZipper.ZippingDecider getZippingDecider() {
         if (getProjectType() == TmcProjectType.JAVA_MAVEN) {
-            return new MavenZippingDecider(getTmcProjectFile());
+            return new MavenZippingDecider(this);
         } else {
-            return new DefaultZippingDecider(getTmcProjectFile());
+            return new DefaultZippingDecider(this);
         }
     }
-    
+
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof TmcProjectInfo) {
-            return this.project.equals((TmcProjectInfo)obj);
+            return this.project.equals((TmcProjectInfo) obj);
         } else {
             return false;
         }
     }
-    
+
     @Override
     public int hashCode() {
         return project.hashCode();
     }
-    
+
     private abstract static class AbstractZippingDecider implements RecursiveZipper.ZippingDecider {
-        protected TmcProjectFile projectFile;
-        
-        public AbstractZippingDecider(TmcProjectFile projectFile) {
-            this.projectFile = projectFile;
+        protected TmcProjectInfo projectInfo;
+
+        public AbstractZippingDecider(TmcProjectInfo projectInfo) {
+            this.projectInfo = projectInfo;
         }
-    }
-    
-    private static class DefaultZippingDecider extends AbstractZippingDecider {
-        public DefaultZippingDecider(TmcProjectFile projectFile) {
-            super(projectFile);
-        }
-        
+
         @Override
         public boolean shouldZip(String zipPath) {
-            if (projectFile.getExtraStudentFiles().contains(withoutRootDir(zipPath))) {
+            File dir = new File(projectInfo.getProjectDirAsFile().getParentFile(), zipPath);
+            if (!dir.isDirectory()) {
+                return true;
+            }
+            
+            return !new File(dir, ".tmcnosubmit").exists();
+        }
+    }
+
+    private static class DefaultZippingDecider extends AbstractZippingDecider {
+
+        public DefaultZippingDecider(TmcProjectInfo projectInfo) {
+            super(projectInfo);
+        }
+
+        @Override
+        public boolean shouldZip(String zipPath) {
+            if (!super.shouldZip(zipPath)) {
+                return false;
+            }
+
+            if (projectInfo.getTmcProjectFile().getExtraStudentFiles().contains(withoutRootDir(zipPath))) {
                 return true;
             } else {
                 return zipPath.contains("/src/");
             }
         }
-        
+
         private String withoutRootDir(String zipPath) {
             int i = zipPath.indexOf('/');
             if (i != -1) {
@@ -112,16 +128,21 @@ public class TmcProjectInfo {
             }
         }
     }
-    
+
     private static class MavenZippingDecider extends AbstractZippingDecider {
+
         private static final Pattern rejectPattern = Pattern.compile("^[^/]+/(target|lib/testrunner)/.*");
-        
-        public MavenZippingDecider(TmcProjectFile projectFile) {
-            super(projectFile);
+
+        public MavenZippingDecider(TmcProjectInfo projectInfo) {
+            super(projectInfo);
         }
-        
+
         @Override
         public boolean shouldZip(String zipPath) {
+            if (!super.shouldZip(zipPath)) {
+                return false;
+            }
+
             return !rejectPattern.matcher(zipPath).matches();
         }
     }
