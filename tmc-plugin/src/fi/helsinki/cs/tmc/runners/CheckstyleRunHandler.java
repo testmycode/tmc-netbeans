650f7964083ee18fb858a0393e80cee43dea69d2
Kenny Heinonen <kenny_heinonen@hotmail.com>
2014-05-07 15:20:51 +0300

    Add checkstyle as public package

--- tmc-plugin/src/fi/helsinki/cs/tmc/runners/CheckstyleRunHandler.java
+++ tmc-plugin/src/fi/helsinki/cs/tmc/runners/CheckstyleRunHandler.java
@@ -1,10 +1,18 @@
 package fi.helsinki.cs.tmc.runners;
 
+import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
 import fi.helsinki.cs.tmc.stylerunner.CheckstyleRunner;
+import java.io.File;
+import java.util.ArrayList;
+import org.openide.util.Exceptions;
 
 public class CheckstyleRunHandler {
 
     public void performAction() {
-        CheckstyleRunner.check();
+        try {
+            new CheckstyleRunner(new ArrayList<File>()).run();
+        } catch (CheckstyleException ex) {
+            Exceptions.printStackTrace(ex);
+        }
     }
 }
