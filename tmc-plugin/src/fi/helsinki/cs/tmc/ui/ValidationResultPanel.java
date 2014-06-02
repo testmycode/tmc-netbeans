61e00755735434c41865106a100e0c0ceda218ac
Kenny Heinonen <kenny_heinonen@hotmail.com>
2014-06-02 13:38:29 +0300

    Change layout to GridBag - #13

--- tmc-plugin/src/fi/helsinki/cs/tmc/ui/ValidationResultPanel.java
+++ tmc-plugin/src/fi/helsinki/cs/tmc/ui/ValidationResultPanel.java
@@ -4,11 +4,11 @@ import fi.helsinki.cs.tmc.stylerunner.validation.ValidationError;
 import fi.helsinki.cs.tmc.stylerunner.validation.ValidationResult;
 
 import java.awt.Color;
-import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.io.File;
 import java.util.List;
 import java.util.Map.Entry;
-
 import javax.swing.JPanel;
 
 import org.netbeans.api.java.classpath.GlobalPathRegistry;
@@ -16,9 +16,18 @@ import org.openide.filesystems.FileObject;
 
 public final class ValidationResultPanel extends JPanel {
 
+    private final GridBagConstraints constraints = new GridBagConstraints();
+
     public ValidationResultPanel() {
 
-        this.setLayout(new GridLayout(0, 1, 0, 5));
+        constraints.anchor = GridBagConstraints.NORTHWEST;
+        constraints.gridx = 0;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.weightx = 1.0;
+        constraints.weighty = 0.0;
+        constraints.insets.top = 5;
+
+        this.setLayout(new GridBagLayout());
     }
 
     public void setValidationResult(final ValidationResult result) {
@@ -49,14 +58,16 @@ public final class ValidationResultPanel extends JPanel {
                          Color.DARK_GRAY,
                          file.getName(),
                          builder.toString(),
-                         null));
+                         null),
+                         constraints);
             } else {
 
                 this.add(new ResultCell(new Color(0xFFD000),
                          Color.DARK_GRAY,
                          fileObject,
                          builder.toString(),
-                         null));
+                         null),
+                         constraints);
             }
 
         }
