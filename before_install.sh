336861e2932c42a7afa1881ca879bc0cecfc04d0
Pihla Toivanen <toivanenpihla@gmail.com>
2015-07-01 14:05:41 +0300

    before_install script

--- before_install.sh
+++ before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Installs tmc-core
 
-mvn clean install -U -f maven-wrapper/pom.xml
 git clone https://github.com/rage/tmc-core.git
 mvn clean install -U -f tmc-core/pom.xml
+mvn clean install -U -f maven-wrapper/pom.xml
