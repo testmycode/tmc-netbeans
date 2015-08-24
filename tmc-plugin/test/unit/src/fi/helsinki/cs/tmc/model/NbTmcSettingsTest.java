eb18bd9729a164954cc65f32ba9485602f6532a8
Jarmo Isotalo <jamo@isotalo.fi>
2015-08-24 13:34:12 +0300

    Rename test to reflect changes done in the tested class

--- tmc-plugin/test/unit/src/fi/helsinki/cs/tmc/model/NbTmcSettingsTest.java
+++ tmc-plugin/test/unit/src/fi/helsinki/cs/tmc/model/NbTmcSettingsTest.java
@@ -11,97 +11,97 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import static org.mockito.Mockito.*;
 
-public class TmcSettingsTest {
+public class NbTmcSettingsTest {
     private PersistableSettings store;
     @Mock private Tailoring tailoring;
     private TmcEventBus eventBus;
-    
+
     private NbTmcSettings settings;
-    
+
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         store = PersistableSettings.forModule(this.getClass());
         settings = newSettings();
         eventBus = TmcEventBus.createNewInstance();
-        
+
         when(tailoring.getDefaultServerUrl()).thenReturn("http://default.example.com");
     }
-    
+
     @After
     public void tearDown() {
         store.cancel();
     }
-    
+
     private NbTmcSettings newSettings() {
         return new NbTmcSettings(store, tailoring, eventBus);
     }
-    
+
     @Test
     public void itUsesBaseUrlFromTailoringIfNoneStored() {
         assertEquals("http://default.example.com", settings.getServerAddress());
     }
-    
+
     @Test
     public void itUsesProjectRootDirFromTailoringIfNoneStored() {
         assertEquals("http://default.example.com", settings.getServerAddress());
     }
-    
+
     @Test
     public void itStripsTrailingSlashesOffTheBaseUrl() {
         settings.setServerBaseUrl("http://example.com");
         assertEquals("http://example.com", settings.getServerAddress());
-        
+
         settings.setServerBaseUrl("http://example.com/");
         assertEquals("http://example.com", settings.getServerAddress());
-        
+
         settings.setServerBaseUrl("http://example.com///////");
         assertEquals("http://example.com", settings.getServerAddress());
-        
+
         settings.setServerBaseUrl("http://example.com///////");
         assertEquals("http://example.com", settings.getServerAddress());
     }
-    
+
     @Test
     public void itDoesntStoreThePasswordByDefault() {
         assertFalse(settings.isSavingPassword());
         settings.setPassword("xoo");
         assertEquals("", newSettings().getPassword());
     }
-    
+
     @Test
     public void itMayOptionallyStoreThePassword() {
         settings.setSavingPassword(true);
         settings.setPassword("xoo");
-        
+
         assertTrue(settings.isSavingPassword());
         assertEquals("xoo", newSettings().getPassword());
         assertTrue(newSettings().isSavingPassword());
     }
-    
+
     @Test
     public void itMayStoreThePasswordEvenAfterItHasBeenSet() {
         settings.setPassword("xoo");
         settings.setSavingPassword(true);
-        
+
         assertTrue(settings.isSavingPassword());
         assertEquals("xoo", newSettings().getPassword());
         assertTrue(newSettings().isSavingPassword());
     }
-    
+
     @Test
     public void itCanBeToldToRemoveAStoredPassword() {
         settings.setSavingPassword(true);
         settings.setPassword("xoo");
         settings.setSavingPassword(false);
-        
+
         assertFalse(settings.isSavingPassword());
         assertEquals("xoo", settings.getPassword());
-        
+
         assertEquals("", newSettings().getPassword());
         assertFalse(newSettings().isSavingPassword());
     }
-    
+
     @Test
     public void itSavesWhetherToCheckUpdatesInTheBackground() {
         assertTrue(settings.isCheckingForUpdatesInTheBackground());
@@ -110,7 +110,7 @@ public class TmcSettingsTest {
         settings.setCheckingForUpdatesInTheBackground(true);
         assertTrue(newSettings().isCheckingForUpdatesInTheBackground());
     }
-    
+
     @Test
     public void localeToStringBehavesAsExpected() {
         assertEquals("fi", new Locale("fi").toString());
