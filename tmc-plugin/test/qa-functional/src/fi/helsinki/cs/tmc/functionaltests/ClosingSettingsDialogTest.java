1a880ad29d516b7266b61c260869dcf8a6925b59
Jarmo Isotalo <jamo@isotalo.fi>
2015-09-25 09:44:03 +0300

    Remove trailing whitespace and reorder imports

--- tmc-plugin/test/qa-functional/src/fi/helsinki/cs/tmc/functionaltests/ClosingSettingsDialogTest.java
+++ tmc-plugin/test/qa-functional/src/fi/helsinki/cs/tmc/functionaltests/ClosingSettingsDialogTest.java
@@ -1,21 +1,21 @@
-
 package fi.helsinki.cs.tmc.functionaltests;
+
+import static fi.helsinki.cs.tmc.functionaltests.utils.TmcFunctionalTestCase.loadSuite;
 
 import fi.helsinki.cs.tmc.functionaltests.utils.SettingsOperator;
 import fi.helsinki.cs.tmc.functionaltests.utils.TmcFunctionalTestCase;
-import static fi.helsinki.cs.tmc.functionaltests.utils.TmcFunctionalTestCase.loadSuite;
+
 import junit.framework.Test;
 
 public class ClosingSettingsDialogTest extends TmcFunctionalTestCase{
     public static Test suite() {
         return loadSuite(ClosingSettingsDialogTest.class);
     }
-    
+
     public ClosingSettingsDialogTest() {
         super("ClosingSettingsDialogTest");
     }
-    
-    
+
     public void testClosingSettingsDialog() throws Exception {
         SettingsOperator settings = SettingsOperator.openSettingsDialog();
         settings.closeDialogWindow();
