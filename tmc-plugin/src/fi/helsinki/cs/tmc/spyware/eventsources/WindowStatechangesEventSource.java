a7ca5684e4f69800682cdc1a4a83a7f700d1157a
Jarmo Isotalo <jamo@isotalo.fi>
2015-09-26 16:36:19 +0300

    Listen for changes happening in the editor tabs

--- tmc-plugin/src/fi/helsinki/cs/tmc/spyware/eventsources/WindowStatechangesEventSource.java
+++ tmc-plugin/src/fi/helsinki/cs/tmc/spyware/eventsources/WindowStatechangesEventSource.java
@@ -1,16 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fi.helsinki.cs.tmc.spyware.eventsources;
 
 import fi.helsinki.cs.tmc.model.CourseDb;
 import fi.helsinki.cs.tmc.model.ProjectMediator;
 import fi.helsinki.cs.tmc.spyware.EventReceiver;
-import fi.helsinki.cs.tmc.spyware.EventSendBuffer;
 import fi.helsinki.cs.tmc.spyware.LoggableEvent;
 import fi.helsinki.cs.tmc.utilities.JsonMaker;
+
+import org.openide.windows.WindowManager;
+import org.openide.windows.Mode;
+
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.beans.PropertyChangeEvent;
@@ -18,9 +16,12 @@ import java.beans.PropertyChangeListener;
 import java.nio.charset.Charset;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.openide.windows.WindowManager;
-import org.openide.windows.Mode;
-import org.openide.windows.WindowSystemEvent;
+import javax.accessibility.Accessible;
+import javax.swing.JEditorPane;
+import javax.swing.text.Document;
+import javax.swing.text.JTextComponent;
+import org.netbeans.api.editor.EditorRegistry;
+import org.openide.filesystems.FileObject;
 
 public final class WindowStatechangesEventSource implements PropertyChangeListener, WindowListener {
 
@@ -40,6 +41,7 @@ public final class WindowStatechangesEventSource implements PropertyChangeListen
     void startListening() {
         log.log(Level.INFO, "Attaching to listen WindowEventProperties");
         WindowManager.getDefault().addPropertyChangeListener(this);
+        EditorRegistry.addPropertyChangeListener(this);
         WindowManager.getDefault().getMainWindow().addWindowListener(this);
     }
 
@@ -49,23 +51,44 @@ public final class WindowStatechangesEventSource implements PropertyChangeListen
      */
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        String data = JsonMaker.create()
-                .add("name", evt.getPropertyName())
-                .add("new_value", toStringWithObjects(evt.getNewValue()))
-                .add("old_value", toStringWithObjects(evt.getOldValue()))
-                .add("propagation_id", toStringWithObjects(evt.getPropagationId()))
-                .toString();
+        JTextComponent jtc = EditorRegistry.lastFocusedComponent();
+        Document d = null;
+        FileObject fd = null;
+        if (jtc != null) {
+            d = jtc.getDocument();
+            if (d instanceof FileObject) {
+                fd = ((FileObject) d);
+            }
+            // use the document
+        }
 
-        LoggableEvent event = new LoggableEvent(courseDb.getCurrentCourse(), "window_event", data.getBytes(Charset.forName("UTF-8")));
-        receiver.receiveEvent(event);
-    }
+    String data = JsonMaker.create()
+            .add("name", evt.getPropertyName())
+            .add("new_value", toStringWithObjects(evt.getNewValue()))
+            .add("old_value", toStringWithObjects(evt.getOldValue()))
+            .add("fd", toStringWithObjects(fd))
+            .add("propagation_id", toStringWithObjects(evt.getPropagationId()))
+            .add("document", toStringWithObjects(d))
+            .add("jtc", toStringWithObjects(jtc))
+            .toString();
 
-    private String toStringWithObjects(Object object) {
+    LoggableEvent event = new LoggableEvent(courseDb.getCurrentCourse(), "window_event", data.getBytes(Charset.forName("UTF-8")));
+
+    receiver.receiveEvent (event);
+}
+
+private String toStringWithObjects(Object object) {
         if (object == null) {
             return "null";
         }
-        if (object instanceof Mode) {
+        if (object instanceof FileObject){
+            return ((FileObject) object).getName() + "--" + ((FileObject) object).getPath();
+        }else if (object instanceof Mode) {
             return ((Mode) object).getName();
+        } else if (object instanceof Accessible) {
+            return ((Accessible) object).getAccessibleContext().getAccessibleName();
+        } else if (object instanceof JEditorPane) {
+            return ((JEditorPane) object).getAccessibleContext().getAccessibleName();
         }
         log.log(Level.WARNING, "[spyware] Add support for toStringing class: {0}",
                 object.getClass().getName());
@@ -73,37 +96,37 @@ public final class WindowStatechangesEventSource implements PropertyChangeListen
     }
 
     @Override
-    public void windowOpened(WindowEvent e) {
+        public void windowOpened(WindowEvent e) {
         reactToEvent("window_opened", e);
     }
 
     @Override
-    public void windowClosing(WindowEvent e) {
+        public void windowClosing(WindowEvent e) {
         reactToEvent("window_closing", e);
     }
 
     @Override
-    public void windowClosed(WindowEvent e) {
+        public void windowClosed(WindowEvent e) {
         reactToEvent("window_closed", e);
     }
 
     @Override
-    public void windowIconified(WindowEvent e) {
+        public void windowIconified(WindowEvent e) {
         reactToEvent("window_iconified", e);
     }
 
     @Override
-    public void windowDeiconified(WindowEvent e) {
+        public void windowDeiconified(WindowEvent e) {
         reactToEvent("window_deiconified", e);
     }
 
     @Override
-    public void windowActivated(WindowEvent e) {
+        public void windowActivated(WindowEvent e) {
         reactToEvent("window_activated", e);
     }
 
     @Override
-    public void windowDeactivated(WindowEvent e) {
+        public void windowDeactivated(WindowEvent e) {
         reactToEvent("window_deactivated", e);
     }
 
