fd60ec71b30f0a97fcf8e1217b96bbd9891a7af7
Jarmo Isotalo <jamo@isotalo.fi>
2016-07-25 12:25:51 +0300

    Spyware refactorings

--- tmc-plugin/src/fi/helsinki/cs/tmc/events/TmcEvent.java
+++
@@ -1,7 +0,0 @@
-package fi.helsinki.cs.tmc.events;
-
-/**
- * Marker interface for events.
- */
-public interface TmcEvent {
-}
--- tmc-plugin/src/fi/helsinki/cs/tmc/spyware/SpywareSettings.java
+++
@@ -1,6 +0,0 @@
-package fi.helsinki.cs.tmc.spyware;
-
-public interface SpywareSettings {
-    public boolean isSpywareEnabled();
-    public boolean isDetailedSpywareEnabled();
-}
