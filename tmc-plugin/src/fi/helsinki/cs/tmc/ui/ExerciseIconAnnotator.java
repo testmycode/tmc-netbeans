edc60cab50efc37e5373043a92224da9d485e04a
Henrik Nygren <nygrenh@gmail.com>
2017-02-13 17:33:02 +0200

    Show the green dot also on expired exercises

--- tmc-plugin/src/fi/helsinki/cs/tmc/ui/ExerciseIconAnnotator.java
+++ tmc-plugin/src/fi/helsinki/cs/tmc/ui/ExerciseIconAnnotator.java
@@ -100,10 +100,10 @@ public class ExerciseIconAnnotator implements ProjectIconAnnotator {
     }
 
     private String annotationIconNameForExercise(Exercise exercise) {
-        if (exercise.hasDeadlinePassed()) {
-            return "expired-project-dot.png";
-        } else if (exercise.isAttempted() && exercise.isCompleted() && exercise.isAllReviewPointsGiven()) {
+        if (exercise.isAttempted() && exercise.isCompleted() && exercise.isAllReviewPointsGiven()) {
             return "green-project-dot.png";
+        } else if (exercise.hasDeadlinePassed()) {
+            return "expired-project-dot.png";
         } else if (exercise.isAttempted() && exercise.isCompleted()) {
             return "yellow-project-dot.png";
         } else if (exercise.isAttempted()) {
