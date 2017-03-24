04badfe093f12b14bc2de0c8615f17212c3efb4e
Antti Leinonen <antti.leinonen@helsinki.fi>
2017-03-24 11:58:17 +0200

    Add logger warning if can't detect outdated netbeans

--- tmc-plugin/src/fi/helsinki/cs/tmc/actions/CheckForOutdatedNetbeans.java
+++ tmc-plugin/src/fi/helsinki/cs/tmc/actions/CheckForOutdatedNetbeans.java
@@ -2,14 +2,19 @@ package fi.helsinki.cs.tmc.actions;
 
 import fi.helsinki.cs.tmc.core.holders.TmcSettingsHolder;
 import fi.helsinki.cs.tmc.ui.TmcNotificationDisplayer;
+
+import java.awt.HeadlessException;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+
 import org.openide.awt.NotificationDisplayer;
 import org.openide.util.ImageUtilities;
 
 public class CheckForOutdatedNetbeans {
     
     private static final String OUTDATED_TITLE = "Please update your Netbeans.";
-    private static final String OUTDATED_MESSAGE = "Your Netbeans is too old and doesn't work with the TestMyCode plugin.\nWe recommend that you update your Netbeans to at least version 8.2.";
+    private static final String OUTDATED_MESSAGE = "Your Netbeans is too old and it may not work with the TestMyCode plugin.\nWe recommend that you update your Netbeans to at least version 8.2.";
+    private static final Logger logger = Logger.getLogger(CheckForOutdatedNetbeans.class.getName());
     
     public static void run() {
         TmcNotificationDisplayer.SingletonToken NOTIFIER_TOKEN = TmcNotificationDisplayer.createSingletonToken();
@@ -18,10 +23,10 @@ public class CheckForOutdatedNetbeans {
             final String hostProgramVersion = TmcSettingsHolder.get().hostProgramVersion().substring(0, 4);
             final int version = Integer.parseInt(hostProgramVersion);
             if (version > 2000 && version < 2015) {
-                JOptionPane.showMessageDialog(null, OUTDATED_MESSAGE, OUTDATED_TITLE, JOptionPane.ERROR_MESSAGE);
                 notifier.notify(NOTIFIER_TOKEN, OUTDATED_TITLE, ImageUtilities.loadImageIcon("fi/helsinki/cs/tmc/smile.gif", false), OUTDATED_MESSAGE, null, NotificationDisplayer.Priority.HIGH);
             }
-        } catch (Exception ex) {
+        } catch (HeadlessException | NumberFormatException ex) {
+            logger.warning("Could not check for an outdated Netbeans.");
         }
     }
 }
