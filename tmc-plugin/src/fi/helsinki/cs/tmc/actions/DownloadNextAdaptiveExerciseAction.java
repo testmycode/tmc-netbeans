45eb513929544a34eb30a3b3ce8fc2e0b079f2a9
Piia <piia.hartikka@helsinki.fi>
2017-05-18 12:19:48 +0300

    Created empty DownloadNextAdaptiveExercise.java

---
+++ tmc-plugin/src/fi/helsinki/cs/tmc/actions/DownloadNextAdaptiveExerciseAction.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fi.helsinki.cs.tmc.actions;
+
+
+
+/**
+ *
+ * @author Piia Hartikka
+ * 
+ * 
+ * Opin seuraavista:
+ * 
+ * - DownloadExercisesAction
+ * Lataa kaikki kurssin tehtävät... Siitä voisi ottaa mallia yhden tehtävän lataamiseen.
+ * 
+ * - DownloadSolutionAction
+ * Sisältää nappulaan liittyvää toiminnallisuutta, josta voisi ottaa mallia.
+ * 
+ */
+
+
+public class DownloadNextAdaptiveExerciseAction {
+   
+}
